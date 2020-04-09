6203fa315de0938ae50c5f38b473ab8dbaa0e4d0
Connor Sullivan <sulliamr@bc.edu>
2020-04-09 19:22:07 -0400

    9.2

--- Snacktacular/SpotsListViewController.swift
+++ Snacktacular/SpotsListViewController.swift
@@ -13,7 +13,47 @@ class SpotsListViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var spots: Spots!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        spots = Spots()
+        spots.spotArray.append(Spot(name: "Charles River", address: "483 Dedham Street", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
+        spots.spotArray.append(Spot(name: "Chick Fil a", address: "Westie", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
+        spots.spotArray.append(Spot(name: "Pizza Etc", address: "Oak Square", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowSpot" {
+            let destination = segue.destination as! SpotDetailViewController
+            let selectedIndexPath = tableView.indexPathForSelectedRow!
+            destination.spot = spots.spotArray[selectedIndexPath.row]
+        } else {
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                tableView.deselectRow(at: selectedIndexPath, animated: true)
+            }
+        }
+    }
+}
+
+extension SpotsListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        spots.spotArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SpotsTableViewCell
+        cell.nameLabel.text = spots.spotArray[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    
 }
