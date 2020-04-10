450a5f65c602cb062a569d93d4680b00d8d5ed9d
Connor Sullivan <sulliamr@bc.edu>
2020-04-10 14:23:24 -0400

    9.4
    Instead of pasting the APIkey into the app delegate file as was done in the video I followed the procedure we used in WeatherGift. I created a .gitignore file, added an APIkeys swift file and put my google places API key there

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class SpotDetailViewController: UIViewController {
     
@@ -28,11 +29,24 @@ class SpotDetailViewController: UIViewController {
         addressField.text = spot.address
         
     }
+    
+    func updateUserInterface() {
+        nameField.text = spot.name
+        addressField.text = spot.address
+    }
 
     @IBAction func photoButtonPressed(_ sender: UIButton) {
     }
     
     @IBAction func reviewButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func lookupPlacePressed(_ sender: UIBarButtonItem) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        
+        // Display the autocomplete view controller.
+        present(autocompleteController, animated: true, completion: nil)
     }
     
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
@@ -46,4 +60,28 @@ class SpotDetailViewController: UIViewController {
             navigationController?.popViewController(animated: true)
         }
     }
+}
+
+extension SpotDetailViewController: GMSAutocompleteViewControllerDelegate {
+    
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        spot.name = place.name ?? ""
+        spot.address = place.formattedAddress ?? ""
+        spot.coordinate = place.coordinate
+        dismiss(animated: true, completion: nil)
+        updateUserInterface()
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
+    }
+    
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
