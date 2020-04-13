f2eb182ee2dbeecd47c3c58e21897d7b2b3ccbff
Connor Sullivan <sulliamr@bc.edu>
2020-04-13 14:10:48 -0400

    9.7

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import GooglePlaces
+import MapKit
+import Contacts
 
 class SpotDetailViewController: UIViewController {
     
@@ -16,23 +18,47 @@ class SpotDetailViewController: UIViewController {
     @IBOutlet weak var averageRatingLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var mapView: MKMapView!
     
     var spot: Spot!
+    let regionDistance: CLLocationDistance = 750
+    var locationManager: CLLocationManager!
+    var currentLocation: CLLocation!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //mapView.delegate = self
+        
         if spot == nil {
             spot = Spot()
+            
         }
-        nameField.text = spot.name
-        addressField.text = spot.address
+        let region = MKCoordinateRegion(center: spot.coordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
+        mapView.setRegion(region, animated: true)
         
+        updateUserInterface()
+        
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alertContorller = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertContorller.addAction(alertAction)
+        present(alertContorller, animated: true, completion: nil)
     }
     
     func updateUserInterface() {
         nameField.text = spot.name
         addressField.text = spot.address
+        updateMap()
+    }
+    
+    func updateMap() {
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotation(spot)
+        mapView.setCenter(spot.coordinate, animated: true)
     }
     
     func leaveViewController() {
@@ -94,6 +120,6 @@ extension SpotDetailViewController: GMSAutocompleteViewControllerDelegate {
     func wasCancelled(_ viewController: GMSAutocompleteViewController) {
         dismiss(animated: true, completion: nil)
     }
-    
-    
 }
+
+
